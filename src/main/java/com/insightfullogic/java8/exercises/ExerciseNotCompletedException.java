b9ff6e4ca2d0a573a8d355894d28223c6dec11af
epopov <eugeniu.popov@endava.com>
2019-03-11 09:38:49 +0200

    test

--- src/main/java/com/insightfullogic/java8/exercises/ExerciseNotCompletedException.java
+++ src/main/java/com/insightfullogic/java8/exercises/ExerciseNotCompletedException.java
@@ -3,6 +3,7 @@ package com.insightfullogic.java8.exercises;
 public class ExerciseNotCompletedException extends RuntimeException {
 
     public ExerciseNotCompletedException() {
+
         super("Please remove this line of code and implement the exercise");
     }
 
