d6661fb788adebf055dd7f8e68d6ac0e1d98f166
Alexandr Binkovski <alexandr.binkovski@endava.com>
2019-03-12 15:24:46 +0200

    Test implementation

--- src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
@@ -3,17 +3,23 @@ package com.insightfullogic.java8.exercises.chapter5;
 import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.exercises.Exercises;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import static java.util.Comparator.comparing;
 
 public class LongestName {
 
     public static Artist byReduce(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream()
+//            .max(Comparator.comparing(artist->artist.getName().length()))
+//            .reduce((artist, artist2) -> {
+//                if(artist.getName().length() > artist2.getName().length()) {
+//                    return artist;
+//                } else {
+//                    return artist2;
+//                }
+//            })
+            .reduce((artist, artist2) ->
+                (artist.getName().length() > artist2.getName().length()) ? artist: artist2)
+            .orElse(null);
     }
 
     public static Artist byCollecting(List<Artist> artists) {
